be82a6b1cbbadeee21e8944f41b904102b3d9ec2
kolinzo1 <kolaoyedele@icloud.com>
2024-10-30 21:00:12 -0400

    corrected README.md

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function searchGiphy() {
   const apiKey = 'ICkXapc1xcA0ZEAUGVQdQUoDPlvItBdJ';
   const input = document.getElementById('searchInput').value;
-  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(input)}&api_key=${apiKey}&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(input)}&api_key=${apiKey}&limit=33`;
 
   fetch(url)
       .then(response => response.json())
